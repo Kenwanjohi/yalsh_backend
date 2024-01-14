53dcf681f9723393b9ecb9d73bc5fd8bef8c330e
Kenwanjohi <wanjohiken85@gmail.com>
2024-01-15 01:23:31 +0300

    Try gRPC call from gateway

--- gateway/routes/accounts/routes.js
+++ gateway/routes/accounts/routes.js
@@ -1,5 +1,21 @@
 const fp = require("fastify-plugin");
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const path = require("node:path");
 
+const ProtoFilePath = path.join(
+  __dirname,
+  "../../node_modules/yalsh_protos/dist/accounts.proto"
+);
+const packageDefinition = protoLoader.loadSync(ProtoFilePath);
+
+const { Accounts: AccountsClient } =
+  grpc.loadPackageDefinition(packageDefinition).accountsPackage;
+
+const client = new AccountsClient(
+  "localhost:50052",
+  grpc.credentials.createInsecure()
+);
 module.exports = fp(
   async function accounts(fastify, opts) {
     // Register new user
@@ -22,10 +38,17 @@ module.exports = fp(
         },
       },
       function registerHandler(request, reply) {
-        console.log(request.body);
         // gRPC call CreateUser
-        reply.code(201);
-        return { registered: true };
+        client.createUser(request.body, (err, res) => {
+          if (err) {
+            this.log.error(err);
+            reply.status(500).send({ registered: false });
+          } else {
+            // TODO: Optionally create access_token??
+            console.log(res);
+            reply.code(201).send({ registered: true });
+          }
+        });
       }
     );
   },
