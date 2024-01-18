524d8dfca71777469201e720ac314e280c92f01e
Kenwanjohi <wanjohiken85@gmail.com>
2024-01-18 22:20:40 +0300

    Yeah, not clean, just gotta make it work for now

--- accounts/src/adapters/db/db.ts
+++ accounts/src/adapters/db/db.ts
@@ -1,9 +1,14 @@
 import postgres from "postgres";
 import { IDatabasePort } from "../../ports/db";
-import { User, ProfileUser } from "../../application/entities/account";
+import {
+  User,
+  ProfileUser,
+  UserUpdate,
+} from "../../application/entities/account";
 
 export class AccountsDataSource implements IDatabasePort {
   sql: postgres.Sql;
+
   constructor() {
     try {
       this.sql = postgres(`${process.env.PG_CONN}`);
@@ -11,6 +16,7 @@ export class AccountsDataSource implements IDatabasePort {
       throw error;
     }
   }
+
   async saveUser(user: User): Promise<ProfileUser> {
     const newUser = await this.sql<ProfileUser[]>`
     INSERT INTO users
@@ -18,5 +24,49 @@ export class AccountsDataSource implements IDatabasePort {
     returning user_id, username
     `;
     return newUser[0];
+  }
+
+  async getUser(user_id: number): Promise<User> {
+    const user = await this.sql<
+      User[]
+    >`SELECT username, email, password FROM users WHERE user_id=${user_id}`;
+    return user[0];
+  }
+
+  async updateUser(user: UserUpdate): Promise<boolean> {
+    let updatedUser: {
+      username?: string;
+      email?: string;
+      password?: string;
+    } = {};
+
+    const { user_id, new_password, ...rest } = user;
+
+    updatedUser = rest;
+
+    const withValues: {
+      username?: string;
+      email?: string;
+      password?: string;
+    } = {};
+
+    for (const key in updatedUser) {
+      const value = updatedUser[key];
+      if (value) {
+        withValues[key] = value;
+      }
+    }
+    console.log(withValues, "WITH PROPERTIES");
+    try {
+      const res = await this.sql`
+        UPDATE users set ${this.sql(
+          withValues
+        )} where user_id=${user_id} returning user_id
+      `;
+
+      return res[0]?.user_id ? true : false;
+    } catch (error) {
+      return false;
+    }
   }
 }
