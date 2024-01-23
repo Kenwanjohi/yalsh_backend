49688c24077e9d56127ca11884b1864cde977141
Kenwanjohi <wanjohiken85@gmail.com>
2024-01-24 01:34:13 +0300

    Oh CORS!

--- gateway/plugins/cors.js
+++ gateway/plugins/cors.js
@@ -3,5 +3,6 @@ module.exports = fp(async function (fastify, opts) {
   fastify.register(require("@fastify/cors"), {
     origin: "http://localhost:3000",
     credentials: true,
+    methods: ["POST", "GET", "PATCH", "DELETE"],
   });
 });
