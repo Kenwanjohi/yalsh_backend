2d1d7a504091ebfc4a42c4203b75e9de9108f7e3
Kenwanjohi <wanjohiken85@gmail.com>
2024-01-31 00:07:36 +0300

    connect db in class

--- links/src/adapters/db/db.ts
+++ links/src/adapters/db/db.ts
@@ -1,18 +1,28 @@
 import postgres from "postgres";
 import { LinkItem } from "../../application/entities/link.js";
 import { GetLinks, IDatabasePort } from "../../ports/db.js";
-const sql = postgres(`${process.env.PG_CONN}`);
+import "dotenv/config";
+
 export class LinkDataSource implements IDatabasePort {
+  sql: postgres.Sql;
+
+  constructor() {
+    try {
+      this.sql = postgres(`${process.env.PG_CONN}`);
+    } catch (error) {
+      throw error;
+    }
+  }
   async saveLink(link: LinkItem): Promise<number> {
-    const newLink = await sql`
+    const newLink = await this.sql`
     INSERT INTO links
-     ${sql(link)}
+     ${this.sql(link)}
      returning link_id
     `;
     return newLink[0].link_id;
   }
   async getLinks(userId: number): Promise<GetLinks[]> {
-    const links = await sql<
+    const links = await this.sql<
       GetLinks[]
     >`SELECT link_id, url, key, clicks, expires_at FROM links WHERE user_id=${userId}`;
     return links;
