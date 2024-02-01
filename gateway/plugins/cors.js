2b00499e60d932a0933b303b398b26440f65337c
Kenwanjohi <wanjohiken85@gmail.com>
2024-02-01 20:24:20 +0300

    Fix cors

--- gateway/plugins/cors.js
+++ gateway/plugins/cors.js
@@ -1,7 +1,7 @@
 const fp = require("fastify-plugin");
 module.exports = fp(async function (fastify, opts) {
   fastify.register(require("@fastify/cors"), {
-    origin: "http://localhost:3000",
+    origin: ["http://dev.localhost:3000", "https://app.yalsh.co"],
     credentials: true,
     methods: ["POST", "GET", "PATCH", "DELETE"],
   });
