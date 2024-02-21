4a1a9f6fb1310ad50a9cfd5e3f2e0a8da0688f77
Kenwanjohi <wanjohiken85@gmail.com>
2024-02-22 00:59:57 +0300

    Add @fastify/http-proxy

--- gateway/app.js
+++ gateway/app.js
@@ -1,26 +1,35 @@
-'use strict'
+"use strict";
 
-const path = require('node:path')
-const AutoLoad = require('@fastify/autoload')
+const path = require("node:path");
+const AutoLoad = require("@fastify/autoload");
+const proxy = require("@fastify/http-proxy");
 
 // Pass --options via CLI arguments in command to enable these options.
-const options = {}
+const options = {};
 
 module.exports = async function (fastify, opts) {
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
 
-module.exports.options = options
+  fastify.register(proxy, {
+    upstream: "http://127.0.0.1:3000",
+    prefix: "/api",
+    preHandler: async function (request, reply) {
+      await fastify.authenticate(request, reply);
+    },
+  });
+};
+
+module.exports.options = options;
